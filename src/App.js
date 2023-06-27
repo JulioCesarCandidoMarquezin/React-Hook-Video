19a0694431a4270b9a0c4ec9423966e50f5d29ef
JulioCesarCandidoMarquezin <juliocesarmarquezin@gmail.com>
2023-06-27 16:56:35 -0400

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+
+export default function App() {
+  const [location, setLocation] = useState({});
+
+  useEffect(() => {
+    const watchId = navigator.geolocation.watchPosition(handlePositionReceived);
+    return () => navigator.geolocation.clearWatch(watchId);
+  }, []);
+
+  function handlePositionReceived({ coords }) {
+    const { latitude, longitude } = coords;
+    setLocation({ latitude, longitude });
+  }
+
+  return (
+    <>
+      latitude: {location.latitude} <br />
+      longitude: {location.longitude}
+    </>
+  );
+}
